ce4ed7c7517f11159d1a578f8fe232362e8a97f6
Kishan Mundha <kishan.mundha@gmail.com>
2025-02-16 00:28:49 +0900

    Add Clarity tracking script to production head tags

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,6 +138,20 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  headTags: process.env.NODE_ENV === "production" ? [{
+    tagName: 'script',
+    attributes: {
+      type: 'text/javascript',
+    },
+    innerHTML: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qa9a0mcwuf");
+    `
+  }] : [],
 };
 
 export default config;
